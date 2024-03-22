40d29a0d25a903a54dbd187889c6a68f4333c833
Aswanthcp <cpaswanthpalayad@gmail.com>
2024-03-22 22:13:46 +0530

    live link added

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,4 +1,5 @@
-export const baseUrl = "http://127.0.0.1:8000/api/";
+// export const baseUrl = "http://127.0.0.1:8000/api/";
+export const baseUrl = "https://justpark-backend.onrender.com/api/";
 
 export const userLogin = "login/";
 export const userSignUP = "signup/";
